a83cbca27b50ed5425dbea3e4fe3df105f6233bc
John Funk <email@johnfunk.com>
2023-12-10 14:51:59 -0700

    Externalize sensor data & IFTTT key (feat) Sensor data and IFTTT key moved to environment variables. Improves maintainability, flexibility, and security.

--- print_sensor_data.py
+++ print_sensor_data.py
@@ -1,54 +1,85 @@
 #!/usr/bin/env python
+import os
 import datetime
+
 import requests
 import logging
+import yaml
+
 # from sense_hat import SenseHat
 from sense_emu import SenseHat
 
-logging.getLogger().setLevel(logging.ERROR)
-sense = SenseHat()
-logging.getLogger().setLevel(logging.WARNING)
-sense.clear()
 
-print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),end=',')
+class greenhouseMonitorApp():
+    def __init__(self):
+        self.temperature_f = 0
+        self.pressure = 0
+        self.humidity = 0
 
-raw_low=7.22
-raw_hi=36.11
-raw_range=raw_hi-raw_low
+        logging.getLogger().setLevel(logging.ERROR)
+        self.sense = SenseHat()
+        logging.getLogger().setLevel(logging.WARNING)
+        self.sense.clear()
 
-ref_low=47
-ref_hi=80
-ref_range=raw_hi-raw_low
+ 
+    def get_sensor_data(self):
+        temp = self.sense.get_temperature()
+        self.temperature_f = round(correct_sensor_data('Temperature', temp))
+        self.temperature_f = self.sense.get_temperature()
+        self.pressure = self.sense.get_pressure()
+        self.humidity = self.sense.get_humidity()
+
+    def report_sensor_data(self):
+        body = {'value1': self.temperature_f, 'value2': self.pressure, 'value3': self.humidity}
+        # print(f'{where},{body}')
+        if (35 <= self.temperature_f < 95) == False:
+            r=send_alert_to_iftt(body=body)
+            print(f'{r}', end=',')
+        else:
+            print('no alarm', end=',')
+        print(f'TempSensor={self.temperature_f}')
 
 
-#temperature_c = sense.get_temperature()
-#temperature_f = ((9/5)*temperature_c)+32
-#temperature_f = round(temperature_f)
+#######################################################################################
+#  a few helper functions that I'll likely re-use in other raspberry pi sensor projects
 
-reading=sense.get_temperature()
-corrected_reading=(((reading - raw_low)*ref_range)/raw_range)+ref_low
-temperature_f = round(corrected_reading)
-print(f'Temp={temperature_f}', end=',')
+def correct_sensor_data(sensor_name: str, reading) -> float:
+    """adjusts the raw sensor reading using a two point calibration as described in the article
+    at: https://learn.adafruit.com/calibrating-sensors/two-point-calibration using the named sensor
+    parameters in config.yaml"""
+    # RAW_LOW = 7.22
+    # RAW_HI = 36.11
+    # raw_range = RAW_HI - RAW_LOW
+    #
+    # REF_LOW = 47
+    # REF_HI = 80
+    # ref_range = RAW_HI - RAW_LOW
+    with open('config.yaml') as f:
+        config_data = yaml.load(f, Loader=yaml.FullLoader)
+        RAW_LOW = config_data['Sensor-Calibration'][sensor_name]['raw_low']
+        RAW_HI = config_data['Sensor-Calibration'][sensor_name]['raw_hi']
+        raw_range = RAW_HI - RAW_LOW
+        REF_LOW = config_data['Sensor-Calibration'][sensor_name]['ref_low']
+        REF_HI = config_data['Sensor-Calibration'][sensor_name]['ref_hi']
+        ref_range = REF_HI - REF_LOW
 
-pressure=sense.get_pressure()
-pressure=round(pressure)
-print(f'Pressure={pressure}', end=',')
+    corrected_reading = (((reading - RAW_LOW) * ref_range) / raw_range) + REF_LOW
+    return corrected_reading
 
-humidity = sense.get_humidity()
-humidity = round(humidity)
-print(f'Humidity={humidity}', end=',')
+def send_alert_to_iftt( body:dict ) -> str:
+    """Send the data in the dictionary to your webhook service using the webhook key specified in the .env file"""
+    from dotenv import load_dotenv
+    load_dotenv()
+    iftt_webhook_key = os.getenv('IFTT_WEBHOOK_KEY')
+    if not iftt_webhook_key:
+        raise ValueError('Missing IFTT_WEBHOOK_KEY in .env file')
 
-#sense.show_message(f'Temp={temperature_f}')
-#sense.show_message(f'Pressure={pressure}')
-#sense.show_message(f'Humidity={humidity}')
+    iftt_webhook_url = f'http://maker.ifttt.com/trigger/temp_event/with/key/{iftt_webhook_key}'
+    r = requests.post(url=iftt_webhook_url, json=body, timeout=5)
+    return str(r)
 
-where='http://maker.ifttt.com/trigger/temp_event/with/key/<insertKey>'
-#where='https://johnfunk.com'
-body={'value1':temperature_f,'value2':pressure,'value3':humidity}
-#print(f'{where},{body}')
-if (35 <=temperature_f <95) ==False:
-	r=requests.post(url=where,json=body,timeout=5)
-	print(f'{r}', end=',')
-else:
-	print('no alarm', end=',')
-print(f'TempSensor={reading}')
+
+if __name__ == "__main__":
+    app = greenhouseMonitorApp()
+    app.get_sensor_data()
+    app.report_sensor_data()
