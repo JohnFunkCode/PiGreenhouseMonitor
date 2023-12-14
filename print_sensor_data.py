242a047ff49ca19d7e91f6142da3805e43059f48
John Funk <email@johnfunk.com>
2023-12-14 13:32:50 -0700

    Added outside temperature Added feature to get the outside temperature using National Weather Service API to pull data from the closes station.

--- print_sensor_data.py
+++ print_sensor_data.py
@@ -12,36 +12,61 @@ from sense_hat import SenseHat
 
 class greenhouseMonitorApp():
     def __init__(self):
-        self.temperature_sensor = 0
-        self.temperature_f = 0
-        self.pressure = 0
-        self.humidity = 0
+        self.inside_temperature_sensor = 0
+        self.inside_temperature_f = 0
+        self.inside_pressure = 0
+        self.inside_humidity = 0
+
+        self.outside_temperature_c = 0
+        self.outside_temperature_f = 0
+        self.outside_pressure = 0
+        self.outside_humidity = 0
 
         logging.getLogger().setLevel(logging.ERROR)
         self.sense = SenseHat()
         logging.getLogger().setLevel(logging.WARNING)
         self.sense.clear()
 
- 
     def get_sensor_data(self):
-        self.temperature_sensor = self.sense.get_temperature()
-        self.temperature_f = round(correct_sensor_data('Temperature', self.temperature_sensor))
-        self.pressure = round(self.sense.get_pressure())
-        self.humidity = round(self.sense.get_humidity())
+        self.inside_temperature_sensor = self.sense.get_temperature()
+        self.inside_temperature_f = round(correct_sensor_data('Temperature', self.inside_temperature_sensor))
+        self.inside_pressure = round(self.sense.get_pressure())
+        self.inside_humidity = round(self.sense.get_humidity())
+
+    def get_outside_data(self):
+        nws_station_id = ""
+        with open('config.yaml') as f:
+            config_data = yaml.load(f, Loader=yaml.FullLoader)
+            nws_station_id = config_data['National-Weather-Service-Station']
+
+        nws_station_url = f'https://api.weather.gov/stations/{nws_station_id}/observations/latest'
+        r = requests.get(url=nws_station_url, timeout=5)
+        if r.status_code == 200:
+            data = r.json()
+            self.outside_temperature_c = round(data['properties']['temperature']['value'])
+            self.outside_temperature_f = round(self.outside_temperature_c * 9 / 5 + 32)
+            self.outside_pressure = round(data['properties']['barometricPressure']['value'])
+            self.outside_humidity = round(data['properties']['relativeHumidity']['value'])
+        else:
+            print(f'Error getting data from {nws_station_url}')
 
     def report_sensor_data(self):
         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), end=',')
-        print(f'Temp={self.temperature_f}', end=',')
-        print(f'Pressure={self.pressure}', end=',')
-        print(f'Humidity={self.humidity}', end=',')
-        body = {'value1': self.temperature_f, 'value2': self.pressure, 'value3': self.humidity}
+        print(
+            f'Inside Temp={self.inside_temperature_f} Inside Pressure={self.inside_pressure} Inside Humidity={self.inside_humidity}',
+            end=',')
+        print(
+            f'Outside Temp={self.outside_temperature_f} Outside Pressure={self.outside_pressure} Outside Humidity={self.outside_humidity}',
+            end=',')
+        body = {'value1': f'in:{self.inside_temperature_f}', 'value2': f'out:{self.outside_temperature_f}',
+                'value3': ''}
         # print(f'{where},{body}')
-        if (35 <= self.temperature_f < 95) == False:
-            r=send_alert_to_iftt(body=body)
+        if (35 <= self.inside_temperature_f < 95) == False:
+            r = send_alert_to_iftt(body=body)
             print(f'{r}', end=',')
         else:
             print('no alarm', end=',')
-        print(f'TempSensor={self.temperature_sensor}')
+        print(f'TempSensor={self.inside_temperature_sensor}')
 
 
 #######################################################################################
@@ -70,7 +95,8 @@ def correct_sensor_data(sensor_name: str, reading) -> float:
     corrected_reading = (((reading - RAW_LOW) * ref_range) / raw_range) + REF_LOW
     return corrected_reading
 
-def send_alert_to_iftt( body:dict ) -> str:
+
+def send_alert_to_iftt(body: dict) -> str:
     """Send the data in the dictionary to your webhook service using the webhook key specified in the .env file"""
     from dotenv import load_dotenv
     load_dotenv()
@@ -86,4 +112,5 @@ def send_alert_to_iftt( body:dict ) -> str:
 if __name__ == "__main__":
     app = greenhouseMonitorApp()
     app.get_sensor_data()
+    app.get_outside_data()
     app.report_sensor_data()
