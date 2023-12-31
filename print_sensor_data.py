db74e24add2e1abbb89d48a55af66b656b551ea4
John Funk <email@johnfunk.com>
2023-12-30 20:17:46 -0700

    Adjusted graph site for my iPhone 14 Pro

--- print_sensor_data.py
+++ print_sensor_data.py
@@ -126,7 +126,7 @@ class greenhouseMonitorApp():
         # graph = df_to_graph.plot(x='Time', y=['Inside Temp', 'Outside Temp','Inside Humidity'], title='Greenhouse Temperature and Humidity', xlabel='Time', ylabel='Temperature (F)', xticks=df_to_graph.index, rot=90, figsize=(20,10), grid=True)
         graph = df_to_graph.plot(x='Time', y=['Inside Temp', 'Outside Temp','Outside Temp openweathermap'],
                                  title='Greenhouse Temperature and Humidity', xlabel='Time', ylabel='Temperature (F)',
-                                 figsize=(20, 10), grid=True, xticks=[])
+                                 figsize=(10, 5), grid=True, xticks=[])
         fig = graph.get_figure()
         fig.savefig('GreenHouseReadingsChart.png')
 
